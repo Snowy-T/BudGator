2fd525d206e68a80536106a7e118f0257ffafd39
Snowy-T <untersulznertimotheus@gmail.com>
2025-12-19 13:05:03 +0100

    change + button position and add home Bar (NOT WORKING)

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -2,36 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../controllers/transaction_provider.dart';
+import 'add_transaction_page.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
     final transactions = ref.watch(transactionsProvider);
+
+    final List<Widget> pages = [
+      // Hier kommen deine Tabs rein
+      Center(child: Text('Home Tab')),
+      Center(child: Text('Transaction Tab')),
+      Center(child: Text('Budget Tab')),
+      Center(child: Text('Stats Tab')),
+    ];
+
     return Scaffold(
       appBar: AppBar(title: const Text('BudGator')),
-      body: transactions.isEmpty
-          ? const Center(child: Text('No transactions yet'))
-          : ListView.builder(
-              itemCount: transactions.length,
-              itemBuilder: (context, index) {
-                final t = transactions[index];
-                return Card(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 4,
-                    horizontal: 8,
-                  ),
-                  child: ListTile(
-                    title: Text(t.title),
-                    subtitle: Text('${t.amount} € • ${t.category}'),
-                    trailing: Text(
-                      '${t.date.day}/${t.date.month}/${t.date.year}',
-                    ),
-                  ),
-                );
-              },
-            ),
+      body: pages[_currentIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.push('/addTransaction'),
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.double_arrow_sharp),
+            label: 'Transactions',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.wallet), label: 'Budget'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Stats'),
+        ],
+      ),
     );
   }
 }
